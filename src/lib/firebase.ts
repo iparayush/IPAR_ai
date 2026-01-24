5ce38da7caa50944b8f9b51db321b9780e9fcf1b
AYUSH IPAR <iparayush@gmail.com>
2026-01-24 06:00:07 +0000

    REPLES PARSAN IMG

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // This is intentionally left blank.
-  // You should replace this with your project's Firebase configuration.
-  // You can find it in the Firebase console.
-  // e.g.
-  // apiKey: "...",
-  // authDomain: "...",
-  // projectId: "...",
-  // storageBucket: "...",
-  // messagingSenderId: "...",
-  // appId: "..."
+  apiKey: "AIzaSyDQZBxQm4-U-CK-LCQxH7Vs45urLQ3_4U0",
+  authDomain: "ayushipar-4a8c5.firebaseapp.com",
+  projectId: "ayushipar-4a8c5",
+  storageBucket: "ayushipar-4a8c5.firebasestorage.app",
+  messagingSenderId: "153361823712",
+  appId: "1:153361823712:web:27ac2109be15af032add85",
+  measurementId: "G-JM2VQ08R2W"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export { app };
+const analytics = getAnalytics(app);
